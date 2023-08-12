0582ba8486f1b7d4edfdc5490fd9ffbd49774e80
KevinED11 <mendez_kael@hotmail.com>
2023-08-11 22:43:34 -0700

    add countdown recursive function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import NoReturn
 
 
+def countdown(n: int) -> NoReturn:
+    print(n)
+    if n > 0:
+        countdown(n - 1)
+
 
 def main() -> NoReturn:
-    ...
+    countdown(5)
 
 
 if __name__ == "__main__":
