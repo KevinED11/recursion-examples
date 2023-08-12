847ea762407fc0ffc082a324b7cd835d3cf216c9
KevinED11 <mendez_kael@hotmail.com>
2023-08-12 15:52:51 -0700

    add flatten recursive function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import NoReturn
+from itertools import chain
 
 
 def countdown(n: int) -> NoReturn:
@@ -11,9 +12,43 @@ def factorial(n: int) -> int:
     return 1 if n <= 1 else n * factorial(n - 1)
 
 
+names = [
+    "Adam",
+    [
+        "Bob",
+        [
+            "Chet",
+            "Cat",
+        ],
+        "Barb",
+        "Bert"
+    ],
+    "Alex",
+    [
+        "Bea",
+        "Bill"
+    ],
+    "Ann"
+]
+
+
+def flatten(lista: list) -> list:
+    result = []
+
+    for item in lista:
+        if isinstance(item, list):
+            result += flatten(lista=item)
+        else:
+            result.append(item)
+
+    return result
+
+
 def main() -> NoReturn:
     countdown(5)
     print(factorial(4))
+
+    print(flatten(names))
 
 
 if __name__ == "__main__":
