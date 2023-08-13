e26ce56b882bec374ef5133600e95ac01cb5a726
KevinED11 <mendez_kael@hotmail.com>
2023-08-13 13:38:56 -0700

    add quicksort recursive function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import statistics
 from typing import NoReturn
 from itertools import chain
 
@@ -49,12 +50,38 @@ def palindrome(text: str) -> bool:
     return text[::-1] == text
 
 
+def quicksort(list_items: list) -> list:
+    if len(list_items) <= 1:
+        return list_items
+
+    pivot = statistics.median(
+        [
+            list_items[0],
+            list_items[len(list_items) // 2],
+            list_items[-1]
+        ]
+    )
+
+    items_less, pivot_items, items_greater = (
+        [n for n in list_items if n < pivot],
+        [n for n in list_items if n == pivot],
+        [n for n in list_items if n > pivot],
+    )
+
+    return (
+            quicksort(items_less) +
+            pivot_items +
+            quicksort(items_greater)
+    )
+
+
 def main() -> NoReturn:
     countdown(5)
     print(factorial(4))
 
     print(flatten(names))
     print(palindrome("oso"))
+    print(quicksort([1, 45, 5, 25, 9, 30, 9, 10]))
 
 
 if __name__ == "__main__":
