bc6efa9bac562a2bd1da0d607f353e159c2d01c4
KevinED11 <mendez_kael@hotmail.com>
2023-08-15 08:23:45 -0700

    add delivery recursive function

--- recursion2.py
+++ recursion2.py
@@ -1,10 +1,18 @@
 
 
 
+def delivery(counter = 0) -> None:
+    if counter == 10:
+        print("santa termino de recorrer las casas")
+        return
+
+    print(f"Deliver to house {counter+1}")
+    delivery(counter+1)
+
 
 
 def main() -> None:
-    ...
+    delivery()
 
 if  __name__ == "__main__":
     main()
