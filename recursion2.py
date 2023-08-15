b4cbe77f8a3883a81e33c3627b1e0495226c76f4
KevinED11 <mendez_kael@hotmail.com>
2023-08-15 08:42:09 -0700

    add factoria_recursive function

--- recursion2.py
+++ recursion2.py
@@ -9,10 +9,21 @@ def delivery(counter = 0) -> None:
     print(f"Deliver to house {counter+1}")
     delivery(counter+1)
 
+def factorial_recursive(n: int) -> int:
+    # Base case: 1! = 1
+    if n == 1:
+        return 1
 
+    # Recursive case: n! = n * (n-1)!
+    else:
+        result = n * factorial_recursive(n-1)
+        print(f"Computing factorial of {n} - need to compute {n-1}!")
+
+        return result
 
 def main() -> None:
     delivery()
+    print(factorial_recursive(4))
 
 if  __name__ == "__main__":
     main()
