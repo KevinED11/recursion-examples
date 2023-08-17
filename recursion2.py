79ce2c21dfe0d16a02930b9de1bc502d2462646c
KevinED11 <mendez_kael@hotmail.com>
2023-08-17 16:41:07 -0700

    add list  sum recursive function

--- recursion2.py
+++ recursion2.py
@@ -22,7 +22,7 @@ def factorial_recursive(n: int) -> int:
         return result
 
 
-def sum_recursive(current_number, accumulated_sum):
+def sum_recursive(current_number: int, accumulated_sum: int) -> int:
     # Base case
     # Return the final state
     if current_number == 11:
@@ -33,9 +33,26 @@ def sum_recursive(current_number, accumulated_sum):
     else:
         return sum_recursive(current_number + 1, accumulated_sum + current_number)
 
+
+def list_sum_recursive(input_list):
+    # Base case
+    if not input_list:
+        return 0
+
+    # Recursive case
+    # Decompose the original problem into simpler instances of the same problem
+    # by making use of the fact that the input is a recursive data structure
+    # and can be deﬁned in terms of a smaller version of itself
+    else:
+        head = input_list[0]
+        smaller_list = input_list[1:]
+        return head + list_sum_recursive(smaller_list)
+
+
 def main() -> None:
     delivery()
     print(factorial_recursive(5))
+    print(sum_recursive(0, 0))
 
 
 if __name__ == "__main__":
