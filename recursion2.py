7dee542bbfad298902aa14e4d39ad6e830ce512b
KevinED11 <mendez_kael@hotmail.com>
2023-08-17 13:55:58 -0700

    add sum recursive function

--- recursion2.py
+++ recursion2.py
@@ -1,6 +1,5 @@
 
 
-
 def delivery(counter = 0) -> None:
     if counter == 10:
         print("santa termino de recorrer las casas")
@@ -8,6 +7,7 @@ def delivery(counter = 0) -> None:
 
     print(f"Deliver to house {counter+1}")
     delivery(counter+1)
+
 
 def factorial_recursive(n: int) -> int:
     # Base case: 1! = 1
@@ -17,13 +17,26 @@ def factorial_recursive(n: int) -> int:
     # Recursive case: n! = n * (n-1)!
     else:
         result = n * factorial_recursive(n-1)
-        print(f"Computing factorial of {n} - need to compute {n-1}!")
+        print(result)
 
         return result
 
+
+def sum_recursive(current_number, accumulated_sum):
+    # Base case
+    # Return the final state
+    if current_number == 11:
+        return accumulated_sum
+
+    # Recursive case
+    # Thread the state through the recursive call
+    else:
+        return sum_recursive(current_number + 1, accumulated_sum + current_number)
+
 def main() -> None:
     delivery()
-    print(factorial_recursive(4))
+    print(factorial_recursive(5))
 
-if  __name__ == "__main__":
-    main()
+
+if __name__ == "__main__":
+        main()
